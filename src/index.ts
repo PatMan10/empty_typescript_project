8eb2995ac13beda34277b1cd1b435fba6166da0a
PatMan10 <PatMan10@protonmail.com>
2019-12-02 16:38:46 +0200

    made some tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import Person from "./models/Person";
 
-function printAll(people: Map<string, Person>) {
-  people.forEach(person => person.greet());
-}
-
 const people: Map<string, Person> = new Map();
+
 people.set("patrick", new Person("Patrick", "Heynes"));
 people.set("kevin", new Person("Kevin", "Black"));
 people.set("dexter", new Person("Dexter", "Morgan"));
-printAll(people);
+people.get("kevin")!.firstName = "some long ass name";
+
+people.forEach(person => person.greet());
--- src/models/Person.ts
+++ src/models/Person.ts
@@ -1,11 +1,27 @@
 class Person {
-  constructor(private firstName: string, private lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+  constructor(private _firstName: string, private _lastName: string) {}
+
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  set firstName(aName: string) {
+    if (aName.length < 4 || aName.length > 10)
+      throw new Error(`name too ${aName.length < 4 ? "short" : "long"}`);
+    this._firstName = aName;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  set lastName(aName: string) {
+    if (aName.length < 4 || aName.length > 10)
+      throw new Error(`name too ${aName.length < 4 ? "short" : "long"}`);
+    this._lastName = aName;
   }
 
   greet() {
-    const x = "asd";
     console.log(`Awe! My name is ${this.firstName} ${this.lastName}.`);
   }
 }
