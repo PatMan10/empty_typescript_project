494794f35844ffd862b37fa5f2f677c7db51e139
PatMan10 <PatMan10@protonmail.com>
2019-12-03 11:24:40 +0200

    added more tsconfig

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ const people: Map<string, Person> = new Map();
 people.set("patrick", new Person("Patrick", "Heynes"));
 people.set("kevin", new Person("Kevin", "Black"));
 people.set("dexter", new Person("Dexter", "Morgan"));
-people.get("kevin")!.firstName = "some long ass name";
+people.get("kevin")!.firstName = "som";
 
 people.forEach(person => person.greet());
