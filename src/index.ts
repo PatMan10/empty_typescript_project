c5c1c364d8a33adebafa152eed1fe020f2550802
PatMan10 <PatMan10@protonmail.com>
2019-12-22 08:00:05 +0200

    added Indexable interface

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import Person from "./models/Person";
+//import Indexable from "./interfaces/Indexable";
 
 const people: Map<string, Person> = new Map();
 
 people.set("patrick", new Person("Patrick", "Heynes"));
 people.set("kevin", new Person("Kevin", "Black"));
 people.set("dexter", new Person("Dexter", "Morgan"));
-people.get("kevin")!.firstName = "som";
 
-people.forEach(person => person.greet());
+const patrick = new Person("Patrick", "Heynes");
+//const p = patrick as Indexable;
+
+Object.keys(patrick).forEach(key => console.log(key));
---
+++ src/interfaces/Indexable.ts
@@ -0,0 +1,3 @@
+export default interface IterableObject {
+  [key: string]: any;
+}
